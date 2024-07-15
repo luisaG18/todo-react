e9246b5b879eb5a59025d1cd9537c81912dee30b
Luisa Giraldo <luiisagiraldori@gmail.com>
2024-07-15 18:51:30 -0500

    Add icon at item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -22,13 +22,33 @@ export function Item({ item, handleItemChecked, checked }) {
         checked={checked}
         readOnly={checked}
       />
-      {!checked ? (
-        <span className="container-item__item">{item}</span>
-      ) : (
-        <span className="container-item__item">
-          <del>{item}</del>
-        </span>
-      )}
+      <div className="container-items">
+        {!checked ? (
+          <span className="container-items__item">{item}</span>
+        ) : (
+          <span className="container-items__item">
+            <del>{item}</del>
+          </span>
+        )}
+        <div className="container-items__icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            width="20px"
+            height="20px"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
+            />
+          </svg>
+        </div>
+      </div>
     </div>
   );
 }
